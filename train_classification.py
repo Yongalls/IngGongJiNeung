5a7c17c2024cb87b976fee50e7b08219ce46023f
jadohu <68580285+jadohu@users.noreply.github.com>
2020-10-24 23:31:01 +0900

    Update train_classification.py

--- train_classification.py
+++ train_classification.py
@@ -8,6 +8,7 @@ import numpy as np
 from sklearn.metrics import f1_score
 import os
 from PIL import Image
+import neptune
 
 class AverageMeter(object):
     def __init__(self):
@@ -51,6 +52,28 @@ class PlantsDataset(torch.utils.data.Dataset):
     img = self.transform(img)
     return img, plant_class, disease_class
 
+class PlantsDataset_test(torch.utils.data.Dataset):
+    def __init__(self, split, file_path, root_dir, transform=None):
+        # /drive/My\ Drive/Colab\ Notebooks/dataset/train/train.tsv'
+        self.dataset = pd.read_csv(os.path.join(root_dir, file_path),
+                                   names=['image'])
+        # fix later to be distributed
+        # if split == 'train':
+        #   self.dataset = dataset.iloc[:12800]
+        # else:
+        #   self.dataset = dataset.iloc[12800:]
+        self.root_dir = root_dir
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.dataset)
+
+    def __getitem__(self, idx):
+        img_path = os.path.join(self.root_dir, self.dataset.iloc[idx, 0])
+        img = Image.open(img_path).convert('RGB')
+        img = self.transform(img)
+        return img
+
 def train(train_loader, model, criterion, optimizer, epoch, use_gpu, log_interval, total_num):
   losses = AverageMeter()
   acces_plant = AverageMeter()
@@ -90,7 +113,9 @@ def train(train_loader, model, criterion, optimizer, epoch, use_gpu, log_interva
 
     if i_batch % log_interval == 0:
       print('Train Epoch:{} [{}/{}] Loss:{:.4f}({:.4f}) f1_plant:{:.3f}({:.3f}) f1_disease:{:.3f}({:.3f}) Accuracy_plant:{:.2f} ({:.2f}) Accuracy_disease:{:.2f} ({:.2f})'.format(epoch, i_batch * batch_size, total_num, losses.val,losses.avg, f1s_plant.val, f1s_plant.avg, f1s_disease.val, f1s_disease.avg, acces_plant.val,acces_plant.avg, acces_disease.val,acces_disease.avg))
-
+      neptune.log_metric("train_losses_avg", losses.avg)
+      neptune.log_metric("train_f1_plant", f1s_plant.avg)
+      neptune.log_metric("train_f1_disease", f1s_disease.avg)
   return losses.avg, f1s_plant.avg, f1s_disease.avg
 
 def validate(val_loader, model, epoch, use_gpu, log_interval, total_num):
@@ -122,8 +147,39 @@ def validate(val_loader, model, epoch, use_gpu, log_interval, total_num):
 
       if i_batch % log_interval == 0:
         print('Validate Epoch:{} [{}/{}] f1_plant:{:.3f}({:.3f}) f1_disease:{:.3f}({:.3f}) Accuracy_plant:{:.2f} ({:.2f}) Accuracy_disease:{:.2f} ({:.2f})'.format(epoch, i_batch * batch_size, total_num, f1s_plant.val, f1s_plant.avg, f1s_disease.val, f1s_disease.avg, acces_plant.val,acces_plant.avg, acces_disease.val,acces_disease.avg))
-
+        neptune.log_metric("val_f1_plant", f1s_plant.avg)
+        neptune.log_metric("val_f1_disease", f1s_disease.avg)
   return f1s_plant.avg, f1s_disease.avg
+
+def test(test_loader, model):
+
+    model.eval()
+    answer_list=[]
+    f = open("answer.csv", "w")
+
+    with torch.no_grad():
+        for i_batch, data in enumerate(test_loader):
+            img = data
+            if use_gpu:
+                img=img.cuda()
+            preds = model(img)
+            pred_plant = torch.softmax(preds[:, :class_plants], dim=1)
+            pred_disease = torch.softmax(preds[:, class_plants:], dim=1)
+            for i in range(preds.shape[0]):
+                pred_plant_num = torch.argmax(pred_plant, dim=1)[i]
+                pred_plant_num = pred_plant_num.item()
+                pred_disease_num = torch.argmax(pred_disease, dim=1)[i]
+                pred_disease_num = pred_disease_num.item()
+                answer=str(i_batch*batch_size+i)+'.jpg'+'_'+str(pred_plant_num)+'_'+str(pred_disease_num)
+                answer_list.append(answer)
+    for i in range(len(answer_list)):
+        f.write(answer_list[i]+'\n')
+    f.close()
+
+    return len(answer_list)
+
+neptune.init('jadohu/IngGongJiNeung', api_token='eyJhcGlfYWRkcmVzcyI6Imh0dHBzOi8vdWkubmVwdHVuZS5haSIsImFwaV91cmwiOiJodHRwczovL3VpLm5lcHR1bmUuYWkiLCJhcGlfa2V5IjoiM2JiNmYzMzQtMDYxYS00ZGVhLTk4NmMtZDY3YjA0NTc2NDAxIn0=')
+neptune.create_experiment(name='Ing')
 
 use_gpu = torch.cuda.is_available()
 if use_gpu:
@@ -170,6 +226,12 @@ val_loader = torch.utils.data.DataLoader(
                                                 transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])),
                   batch_size = batch_size, sampler = valid_sampler)
 
+test_loader = torch.utils.data.DataLoader(
+    PlantsDataset_test('test', "test.tsv", r"C:\Users\jadoh\PycharmProjects\IngGongJiNeung\Dataset\test",
+                  transform=transforms.Compose([transforms.Resize(img_size), transforms.ToTensor(),
+                                                transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                                                     std=[0.229, 0.224, 0.225])])),
+    batch_size=batch_size, shuffle=False)
 
 
 model = models.resnet50(pretrained=True)
@@ -192,3 +254,5 @@ for epoch in range(epochs):
   f1_val_plant, f1_val_disease = validate(val_loader, model, epoch, use_gpu, log_interval, len(valid_idx))
   print("finished training epoch: ", epoch)
   print("loss: ", loss)
+    
+test(test_loader, model)
